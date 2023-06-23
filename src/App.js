229e9079ba3f06e9b3ccf80d92a0d19fa76c2ea2
dimitrov93 <ts.dimitrov93@gmail.com>
2023-06-23 10:58:49 +0300

    theme/color context + buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import TopBar from "./scenes/global/TopBar";
+
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="app">
-        <h1>hi</h1>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <TopBar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
