1d912833b8cf2aa8b9f7b3ea013bb8e729f8a09e
dimitrov93 <ts.dimitrov93@gmail.com>
2023-06-27 10:40:44 +0300

    Data tables

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { ColorModeContext, useMode } from "./theme";
 import TopBar from "./scenes/global/TopBar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-// import Team from "./scenes/team";
-// import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
+import Team from "./scenes/team/index";
+import Invoices from "./scenes/invoices/index";
+
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
 // import Line from "./scenes/line";
@@ -14,6 +14,7 @@ import Dashboard from "./scenes/dashboard";
 // import Geography from "./scenes/geography";
 import { Route, Routes } from "react-router-dom";
 import SideBar from "./scenes/global/Sidebar";
+import Contacts from "./scenes/contacts";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -28,16 +29,17 @@ function App() {
             <TopBar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} />
+              <Route path="/team" element={<Team />} /> 
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
+
+              {/* <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/calendar" element={<Calendar />} />
-              <Route path="/geography" element={<Geography />} /> */}
+              <Route path="/geography" element={<Geography />} />  */}
             </Routes>
           </main>
         </div>
