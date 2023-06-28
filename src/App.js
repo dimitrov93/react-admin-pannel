ac0719a3407639a3bd822b872d3d8765c653ab59
dimitrov93 <ts.dimitrov93@gmail.com>
2023-06-28 10:04:07 +0300

    Accordion

--- src/App.js
+++ src/App.js
@@ -4,13 +4,8 @@ import TopBar from "./scenes/global/TopBar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team/index";
 import Invoices from "./scenes/invoices/index";
-
-// import Bar from "./scenes/bar";
 import Form from "./scenes/form/index";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
+import FAQ from "./scenes/faq/index";
 import { Route, Routes } from "react-router-dom";
 import SideBar from "./scenes/global/Sidebar";
 import Contacts from "./scenes/contacts";
@@ -34,11 +29,11 @@ function App() {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} />
+              <Route path="/faq" element={<FAQ />} />
 
               {/* <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
               <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
